3812db5634734dd723309ca38fba1caade48dde4
Linus Oleander <oleander@users.noreply.github.com>
2022-08-17 12:28:33 +0200

    Add controller

---
+++ app/controllers/partners_controller.rb
@@ -0,0 +1,19 @@
+class PartnersController < ApplicationController
+  #
+  # GET /requests/:request_id/partners
+  #
+  # Responds with JSON
+  def index
+    render json: Request
+        .find(params[:request_id])
+        .partners
+        .order("rating DESC, distance ASC")
+  end
+
+  #
+  # GET /partners/:id
+  #
+  def show
+    render json: Partner.find(params[:id])
+  end
+end
