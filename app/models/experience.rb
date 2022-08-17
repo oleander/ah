9a24006d603f9c79292c81765747bddfeafba1b0
Linus Oleander <oleander@users.noreply.github.com>
2022-08-17 12:35:55 +0200

    Better relational validation

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,4 +3,7 @@
 class Experience < ApplicationRecord
   belongs_to :partner
   belongs_to :material
+
+  validates :partner, presence: true, uniqueness: { scope: :material }
+  validates :material, presence: true, uniqueness: { scope: :partner }
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -3,9 +3,9 @@
 class Partner < ApplicationRecord
   has_many :experiences, dependent: :destroy
   has_many :materials, through: :experiences
+
   validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   validates :operating_radius, numericality: { greater_than_or_equal_to: 0 }
-
   validates :lat, presence: true
   validates :lng, presence: true
 end
